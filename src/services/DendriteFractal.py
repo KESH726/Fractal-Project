fd0ff122a12620c359cfd3282b94cf5840063056
Md Masud karim <msmasud578@gmail.com>
2024-11-19 21:09:58 +0100

    Added DendriteFractal and FractalGenerator classes to the project

---
+++ src/services/DendriteFractal.py
@@ -0,0 +1,6 @@
+class DendriteFractal:
+    def __init__(self,x,i):
+        self.x = x
+        self.i = i
+
+    
---
+++ src/services/FractalGenerator.py
@@ -0,0 +1,14 @@
+
+'''
+<h1>FractalGenerator</h1>
+This class is responsible to generate coordinates of fractals using recursion and fractal formulas.
+@:param x : value of the real number
+@:param i : value of the imaginary number
+'''
+class FractalGenerator:
+    def __init__(self,x, i):
+        self.x = x
+        self.i = i
+
+    def generate(self):
+        pass
