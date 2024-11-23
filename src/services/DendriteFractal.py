47529e6aa5efd997484db961264303a73ed89a42
Md Masud karim <msmasud578@gmail.com>
2024-11-23 11:55:50 +0100

    added FractalGenerator and DendriteFractal

--- src/services/DendriteFractal.py
+++ src/services/DendriteFractal.py
@@ -1,6 +1,29 @@
+import numpy as np
+
 class DendriteFractal:
-    def __init__(self,x,i):
+    def __init__(self,x, i,max_iteration = 500):
         self.x = x
         self.i = i
+        self.x_min, self.x_max = -1.5, 1.5  # Range on the real axis
+        self.y_min, self.y_max = -1.5, 1.5  # Range on the imaginary axis
+        self.width, self.height = 800, 800  # Canvas size
+        self.max_iteration = max_iteration
 
-    
+    def set_max_iteration(self, max_iteration):
+        self.max_iteration = max_iteration
+
+    def generate_julia_set(self,max_iteration, escape_radius):
+        x = np.linspace(self.x_min, self.x_max, self.width)
+        y = np.linspace(self.y_min, self.y_max, self.height)
+        zx, zy = np.meshgrid(x, y)
+
+        c = complex(self.x, self.i)
+        z = zx + 1j * zy
+        fractal = np.zeros(z.shape, dtype=int)
+
+        for i in range(max_iteration):
+            mask = np.abs(z) < escape_radius
+            z[mask] = z[mask]**2 + c
+            fractal[mask] = i
+
+        return fractal
--- src/services/FractalGenerator.py
+++ src/services/FractalGenerator.py
@@ -6,9 +6,29 @@ This class is responsible to generate coordinates of fractals using recursion an
 @:param i : value of the imaginary number
 '''
 class FractalGenerator:
-    def __init__(self,x, i):
+    def __init__(self,x, i,max_iteration = 500):
         self.x = x
         self.i = i
+        self.max_iteration = max_iteration
 
-    def generate(self):
-        pass
+    def set_max_iteration(self, max_iteration):
+        self.max_iteration = max_iteration
+
+    def generate_julia_set(self,max_iteration, escape_radius):
+        x = np.linspace(x_min, x_max, width)
+        y = np.linspace(y_min, y_max, height)
+        zx, zy = np.meshgrid(x, y)
+
+        c = complex(self.x, self.i)
+        z = zx + 1j * zy
+        fractal = np.zeros(z.shape, dtype=int)
+
+        for i in range(max_iteration):
+            mask = np.abs(z) < escape_radius
+            z[mask] = z[mask]**2 + c
+            fractal[mask] = i
+
+        return fractal
+
+
+
