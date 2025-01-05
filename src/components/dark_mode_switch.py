5ed57cb12be9bc42c42243286b05704c86582a5d
tscshawn <tristansh4wn@gmail.com>
2025-01-05 23:48:54 +0100

    fix

--- src/components/dark_mode_switch.py
+++ src/components/dark_mode_switch.py
@@ -1,22 +1,22 @@
 from src.tkreact import Context
 import customtkinter as ctk
    
-def toggle_theme():
-    Context.set_var("dark", not (Context.get_var("dark")))
-
-    match Context.get_var("dark"):
-        case True:
-            ctk.set_appearance_mode("dark")
-        case False:
-            ctk.set_appearance_mode("light")
-        case _:
-            print("wtf")
-    
-def get_current_theme_state():
-    return Context.get_var("dark")    
-
 # Dark Mode Switch Component
 def dark_mode_switch(root, frame):
+    def toggle_theme():
+        Context.set_var("dark", not (Context.get_var("dark")))
+
+        match Context.get_var("dark"):
+            case True:
+                ctk.set_appearance_mode("dark")
+            case False:
+                ctk.set_appearance_mode("light")
+            case _:
+                print("wtf")
+    
+    def get_current_theme_state():
+        return Context.get_var("dark")    
+
     # Put the UI of the button here
     btn_darkmode = ctk.CTkButton(frame, text = 'Dark/Light', command = toggle_theme)
     btn_darkmode.pack(pady = 10)
